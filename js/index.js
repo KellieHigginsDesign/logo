6b3ffe87c24666c0ad1839113279126d0bc5272f
KellieHigginsDesign <48456862+KellieHigginsDesign@users.noreply.github.com>
2019-03-11 16:28:01 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+TweenMax.to('#Rectangle_Top', 4.0, {
+  x: 281,
+  ease: Power1.easeInOut,
+  delay: 1 });
+
+
+TweenMax.to('#Rectangle_Bottom', 4.0, {
+  x: 281,
+  ease: Power1.easeInOut,
+  delay: 1 });
+
+
+TweenMax.to('#Center_Triangle', 4.0, {
+  x: 150,
+  ease: Power1.easeInOut,
+  delay: 1 });
